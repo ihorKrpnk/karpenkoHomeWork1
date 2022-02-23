3f3b39f5ab28bce2bbcc04e70611c26f8cdb8d2d
ihor.karpenko <ihor.krpnk.test@gmail.com>
2022-02-23 22:11:30 +0200

    + one more Else

--- src/lesson2/HypothecHusbandWife.java
+++ src/lesson2/HypothecHusbandWife.java
@@ -4,13 +4,19 @@ public class HypothecHusbandWife {
 
     public static void main(String[] args) {
         int ageHusb = 34;
-        int ageWife = 37;
+        int ageWife = 36;
 
         if (ageWife <= 35) {
             if (ageHusb <= 35) {
                 System.out.println("Mortgage approved!");
+            } else {
+                System.out.println("Mortgage denied!");
             }
-        } else System.out.println("Mortgage denied!");
+        } else {
+            System.out.println("Mortgage denied!");
+        }
     }
 }
+
+
 
