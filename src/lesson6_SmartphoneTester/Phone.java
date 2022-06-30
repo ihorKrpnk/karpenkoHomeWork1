c407dc20ce61fd6a4d9f0097a4e67a6b29574228
ihorKrpnk <ihor.krpnk.test@gmail.com>
2022-06-30 17:48:13 +0300

    6 lesson - fix+

--- src/lesson6_SmartphoneTester/Phone.java
+++ src/lesson6_SmartphoneTester/Phone.java
@@ -2,11 +2,11 @@ package lesson6_SmartphoneTester;
 
 public abstract class Phone {
 
-    abstract void setOs(String os);
+    public abstract void setOs(String os);
 
-    abstract void setChargerType(String chargerType);
+    public abstract void setChargerType(String chargerType);
 
-    abstract void setApplicationStore(String applicationStore);
+    public abstract void setApplicationStore(String applicationStore);
 
     public void dialing() {
         System.out.println("Dialing in progress");
